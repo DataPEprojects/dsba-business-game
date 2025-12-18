d8bb854ce25ad4939314c99efca235e49db0d8df
DataPEprojects <paulelie.prd@gmail.com>
2025-12-18 17:26:52 +0100

    Début du fichier de produits

---
+++ entities/product.py
@@ -0,0 +1,15 @@
+"vide pour l'instant, j'imaginais des trucs spécifiques aux produits"
+PRODUCTS_CONFIG = {
+    "A": {
+        "base_cost": 10,
+        "output_per_line": 200 
+    },
+    "B": {
+        "base_cost": 70,
+        "output_per_line": 15 
+    },
+    "C": {
+        "base_cost": 100,
+        "output_per_line": 10 
+    },
+}
